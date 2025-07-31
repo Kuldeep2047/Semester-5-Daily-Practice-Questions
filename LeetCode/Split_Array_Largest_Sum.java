4328a78b707d9f1da27c4e0f2224c8a0ea7e570c
Kuldeep2047 <kuldeepgarg6993@gmail.com>
2025-07-31 14:02:49 +0530

    Initial commit with LeetCode files

---
+++ LeetCode/Split_Array_Largest_Sum.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+public class Split_Array_Largest_Sum {
+    
+    public static void main(String[] args){
+        int n = 34;
+        int k = 6;
+        int num = num_in_k(n,k);
+        int ans = 0;
+        while(num>0){
+            ans += num%10;
+            num = num/10;
+        }
+        // return ans;
+        System.out.println(ans);
+    }
+    public static int num_in_k(int n,int k){
+        int sum=0;
+        int mul =1;
+        while (n > 0) {
+			int rem = n % k;
+			sum = sum + rem * mul;
+			n = n / k;
+			mul = mul * 10;
+	    }
+        return sum;
+    }
+}
