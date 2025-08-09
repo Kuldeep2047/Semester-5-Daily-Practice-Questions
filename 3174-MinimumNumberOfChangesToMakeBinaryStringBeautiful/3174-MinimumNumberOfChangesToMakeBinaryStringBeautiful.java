d51b03ce15ff22807abffb333f5091f7abcce369
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-08-09 13:37:31 +0530

    Create 3174-MinimumNumberOfChangesToMakeBinaryStringBeautiful

---
+++ 3174-MinimumNumberOfChangesToMakeBinaryStringBeautiful/3174-MinimumNumberOfChangesToMakeBinaryStringBeautiful.java
@@ -0,0 +1,12 @@
+// Last updated: 8/9/2025, 1:37:32 PM
+class Solution {
+    public int minChanges(String s) {
+        int c=0;
+        for(int i=0;i<s.length()-1;i+=2){
+            if(s.charAt(i)!=s.charAt(i+1)){
+                c++;
+            }
+        }
+        return c;
+    }
+}
