1ed2eb20cc990a5ba2e9bed4328d392fb86db23c
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-08-09 13:42:08 +0530

    Create 876-HandOfStraights

---
+++ 876-HandOfStraights/876-HandOfStraights.py
@@ -0,0 +1,13 @@
+# Last updated: 8/9/2025, 1:42:08 PM
+class Solution(object):
+    def isNStraightHand(self, hand, groupSize):
+        c=collections.Counter(hand)
+        n=sorted(c)
+        for i in n:
+            if c[i]>0:
+                for j in range(groupSize)[::-1]:
+                    c[i+j]-=c[i]
+                    if c[i+j]<0:
+                        return False
+        return True        
+        
