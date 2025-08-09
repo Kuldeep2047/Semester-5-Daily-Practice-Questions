9a7d3c8a1143db02c44381d032c85a6fe3ce9214
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-08-09 13:40:34 +0530

    Create 1549-LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit

---
+++ 1549-LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit/1549-LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.py
@@ -0,0 +1,24 @@
+# Last updated: 8/9/2025, 1:40:35 PM
+class Solution(object):
+    def longestSubarray(self, arr, l):
+        i, j, ret = 0, 0, 0
+        q1 = deque()  # increasing order deque
+        q2 = deque()  # decreasing order deque
+
+        for j in range(len(arr)):
+            while q1 and arr[q1[-1]] > arr[j]:
+                q1.pop()
+            q1.append(j)
+            while q2 and arr[q2[-1]] < arr[j]:
+                q2.pop()
+            q2.append(j)
+            while arr[q2[0]] - arr[q1[0]] > l:
+                if q2[0] == i:
+                    q2.popleft()
+                if q1[0] == i:
+                    q1.popleft()
+                i += 1
+
+            ret = max(ret, j - i + 1)
+
+        return ret
