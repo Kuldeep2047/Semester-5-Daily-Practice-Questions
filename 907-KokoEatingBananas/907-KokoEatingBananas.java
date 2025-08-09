3c9cd68062cb81356b788fd60f6234a2d65049ce
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-08-09 13:42:01 +0530

    Create 907-KokoEatingBananas

---
+++ 907-KokoEatingBananas/907-KokoEatingBananas.java
@@ -0,0 +1,33 @@
+// Last updated: 8/9/2025, 1:42:01 PM
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        return minSpeed(piles,h);
+    }
+    public static int minSpeed(int[] piles , int h) {
+		Arrays.sort(piles);
+		int n = piles.length;
+		int start=1;
+		int end= piles[n-1];
+		int ans=end;
+		while(start<=end) {
+			int mid = (start+end)/2;
+			if(isPossible(piles,h,mid)) {
+				ans = mid;
+				end = mid-1;
+			}else {
+				start = mid+1;
+			}
+		}
+		return ans;
+	}
+	public static boolean isPossible(int[] piles, int h, int mid) {
+		int c=0;
+		for(int i=0;i<piles.length;i++) {
+			c += Math.ceil((double)piles[i]/mid);
+			if(c>h) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
