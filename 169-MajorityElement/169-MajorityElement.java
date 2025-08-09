f26dcbf3962e86f60a4377c0f816c48eb39a2d21
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-08-09 13:44:44 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,22 @@
+// Last updated: 8/9/2025, 1:44:45 PM
+class Solution {
+    public int majorityElement(int[] nums) {
+        return Moore_Voting(nums);
+    }
+    public static int Moore_Voting(int[] arr) {
+		int e=arr[0];
+		int v=1;
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]==e) {
+				v++;
+			}else {
+				v--;
+				if(v==0) {
+					e=arr[i];
+					v=1;
+				}
+			}
+		}
+		return e;
+	}
+}
