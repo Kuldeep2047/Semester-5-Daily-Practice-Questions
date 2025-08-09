9d23e5e50d808cbda8428d02d8cd0dab9799302d
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-08-09 13:46:44 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,19 @@
+// Last updated: 8/9/2025, 1:46:44 PM
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        String s = String.valueOf(x);
+        int left=0;
+        int right= s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
