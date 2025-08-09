8809c0d86e8bb63dd2baa6ba90855eb1ebd68504
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-08-09 13:39:02 +0530

    Create 2221-CheckIfAParenthesesStringCanBeValid

---
+++ 2221-CheckIfAParenthesesStringCanBeValid/2221-CheckIfAParenthesesStringCanBeValid.java
@@ -0,0 +1,34 @@
+// Last updated: 8/9/2025, 1:39:02 PM
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        return isvalidParentheses(s,locked);
+    }
+    public static boolean isvalidParentheses(String s,String locked){
+        if(s.length()%2 != 0){
+            return false;
+        }
+        int open=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || locked.charAt(i)=='0'){
+                open++;
+            }else{
+                open--;
+                if(open<0){
+                    return false;
+                }
+            }
+        }
+        int closed=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)==')' || locked.charAt(i)=='0'){
+                closed++;
+            }else{
+                closed--;
+                if(closed<0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
