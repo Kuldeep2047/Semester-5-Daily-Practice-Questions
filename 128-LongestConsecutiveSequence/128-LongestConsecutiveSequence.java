f866fbcb293e05f7f70c1d1f2f1ef64dcda809ae
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-08-27 12:47:19 +0530

    File updated at 8/27/2025, 12:47:18 PM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -1,28 +1,49 @@
-// Last updated: 8/9/2025, 1:45:19 PM
+// Last updated: 8/27/2025, 12:47:18 PM
 class Solution {
     public int longestConsecutive(int[] nums) {
-        return answer(nums);
+        // return answer(nums);
+        return Longest_Consecutive(nums);
     }
-    public static int answer(int[] arr){
-        int ans =0;
-        Set<Integer> set = new HashSet<>();
-        for(int n:arr){
-            set.add(n);
-        }
+    public static int Longest_Consecutive(int[] arr) {
+		HashSet<Integer> set = new HashSet<>();
+		for(int i=0;i<arr.length;i++) {
+			set.add(arr[i]);
+		}
+		int ans =0;
+		for(int i=0;i<arr.length;i++) {
+			if(! set.contains(arr[i]-1)) {
+				int c=0;
+				int key = arr[i];
+				while(set.contains(key)) {
+					set.remove(key);
+					c++;
+					key++;
+				}
+				ans = Math.max(ans, c);
+			}
+		}
+		return ans;
+		
+	}
+    // public static int answer(int[] arr){
+    //     int ans =0;
+    //     Set<Integer> set = new HashSet<>();
+    //     for(int n:arr){
+    //         set.add(n);
+    //     }
 
-        for(int n:set){
-            if(!set.contains(n-1)){
-                int start = n;
-                int len =1;
-                while(set.contains(n+1)){
-                    len++;
-                    n++;
-                }
+    //     for(int n:set){
+    //         if(!set.contains(n-1)){
+    //             // int start = n;
+    //             int len =1;
+    //             while(set.contains(n+1)){
+    //                 len++;
+    //                 n++;
+    //             }
 
-                ans = Math.max(ans,len);
-            }
-        }
-        return ans;
-
-    }
+    //             ans = Math.max(ans,len);
+    //         }
+    //     }
+    //     return ans;
+    // }
 }
