2590509d8907964b997b9427fc95b7dcb0063467
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-08-28 11:17:53 +0530

    File updated at 8/28/2025, 11:17:53 AM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,45 +1,32 @@
-// Last updated: 8/9/2025, 1:46:10 PM
+// Last updated: 8/28/2025, 11:17:53 AM
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        return Group_Anagram(strs);
+    public static List<List<String>> groupAnagrams(String[] arr) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++) {
+        	String key = GetKey(arr[i]);
+        	if(!map.containsKey(key)) {
+        		map.put(key,  new ArrayList<>());
+        	}
+        	map.get(key).add(arr[i]);
+        }
+        List<List<String>> ll = new ArrayList<>();
+        for(String key:map.keySet()) {
+        	ll.add(map.get(key));
+        }
+        return ll;
+        
     }
-    public static List<List<String>> Group_Anagram(String[] strs){
-        List<List<String>> ans = new ArrayList<>();
-        boolean[] visit = new boolean[strs.length];
-        for(int i=0;i<strs.length;i++){
-            if(visit[i]==true){
-                continue;
-            }
-            visit[i] = true;
-            List<String> ll = new ArrayList<>();
-            ll.add(strs[i]);
-            for(int j=i+1;j<strs.length;j++){
-                if(visit[j]==false && isAnagram(strs[i],strs[j])){
-                    ll.add(strs[j]);
-                    visit[j] = true;
-                }
-            }
-            ans.add(ll);
-        } 
-        return ans;
-    }
-    public static boolean isAnagram(String s1,String s2){
-        if(s1.length() != s2.length()){
-            return false;
-        }
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
-        for(int i=0;i<s1.length();i++){
-            arr1[s1.charAt(i)-'a']++;
-        }
-        for(int i=0;i<s2.length();i++){
-            arr2[s2.charAt(i)-'a']++;
-        }
-        for(int i=0;i<26;i++){
-            if(arr1[i] != arr2[i]){
-                return false;
-            }
-        }
-        return true;
-    }
+	
+	public static String GetKey(String s) {
+		int[] freq = new int[26];
+		for(int i=0;i<s.length();i++) {
+			char ch = s.charAt(i);
+			freq[ch-'a']++;
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < freq.length; i++) {
+			sb.append(freq[i]+",");
+		}
+		return sb.toString();
+	}
 }
