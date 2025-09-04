8de6b7c6964aff2e2344ccbf062c9544c7858db4
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-04 12:10:56 +0530

    Create 518-CoinChangeII

---
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -0,0 +1,26 @@
+// Last updated: 9/4/2025, 12:10:56 PM
+class Solution {
+    public int change(int amount, int[] coins) {
+        return Coin_ChangeBU(coins,amount);
+    }
+    public static int Coin_ChangeBU(int[] coin,int amount) {
+		int[][] dp = new int[coin.length+1][amount+1];
+		for(int i=0;i<dp.length;i++) {
+			dp[i][0] = 1;				//base case hai
+		}
+		for (int i = 1; i < dp.length; i++) {
+			for (int am = 1; am < dp[0].length; am++) {
+				int inc = 0;
+				int exc = 0;
+				if(am>= coin[i-1]) {
+					inc = dp[i][am-coin[i-1]];
+				}
+				exc = dp[i-1][am];
+				dp[i][am] = inc + exc;
+			}
+			
+		}
+		return dp[dp.length-1][dp[0].length-1];
+		
+	}
+}
