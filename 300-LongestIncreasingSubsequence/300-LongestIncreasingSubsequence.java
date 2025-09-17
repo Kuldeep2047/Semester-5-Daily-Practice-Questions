a91f14f1ba6599c3f3ec5203a6d6d91d952da6f8
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-17 12:09:18 +0530

    File updated at 9/17/2025, 12:09:18 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,32 +1,35 @@
-// Last updated: 9/3/2025, 12:35:48 PM
+// Last updated: 9/17/2025, 12:09:18 PM
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        // return answer(0,-1,nums,nums.length);
-        return answer2(nums);
+        return LIS(nums);
     }
-    public int answer2(int[] arr){
-        int[] dp = new int[arr.length];
-		Arrays.fill(dp, 1);
+    public static int LIS(int[] arr) {
+		int[] dp = new int[arr.length];
+		int len =1;
+		dp[0] = arr[0];
 		for(int i=1;i<dp.length;i++) {
-			for(int j = i-1; j>=0;j--) {
-				if(arr[i]>arr[j]) {
-					int x = dp[j];
-					dp[i] = Math.max(dp[i], x+1);
-				}
+			if(arr[i] > dp[len-1]) {
+				dp[len] = arr[i];
+                len++;
+			}else {
+				int idx = BinarySearch(dp, 0, len -1, arr[i]);
+				dp[idx] = arr[i];
 			}
 		}
-		return Arrays.stream(dp).max().getAsInt();
-    }
-
-    // = > This give TLE
-    // public int answer(int idx, int prev, int[] arr, int n){
-    //     if(idx == n){
-    //         return 0;
-    //     }
-    //     int len = 0 + answer(idx+1, prev, arr,n);
-    //     if(prev == -1 || arr[idx]>arr[prev]){
-    //         len = Math.max(len , 1 + answer(idx+1, idx, arr, n));
-    //     }
-    //     return len;
-    // }
+		return len;
+	}
+	public static int BinarySearch(int[] dp, int si, int ei, int item) {
+		int idx =0;
+		while(si<=ei) {
+			int mid = (si+ei)/2;
+			if(dp[mid]>=item) {
+				idx = mid;
+				ei = mid-1;
+			}else {
+				si = mid+1;
+			}
+		}
+		return idx;
+		
+	}
 }
