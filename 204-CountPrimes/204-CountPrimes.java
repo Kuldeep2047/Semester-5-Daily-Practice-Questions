078e3d9ea5f4ad1e283741f9b6ec1825080e0a5a
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-26 23:20:20 +0530

    File updated at 9/26/2025, 11:20:19 PM

--- 204-CountPrimes/204-CountPrimes.java
+++ 204-CountPrimes/204-CountPrimes.java
@@ -1,27 +1,28 @@
-// Last updated: 8/9/2025, 1:44:33 PM
+// Last updated: 9/26/2025, 11:20:19 PM
 class Solution {
     public int countPrimes(int n) {
-        if(n<2) {
+        if(n==0 || n==1){
             return 0;
         }
-        int[] prime = new int[n];
-
-        for(int i=2; i*i<=n; i++) {
-            if(prime[i]==0) {
-                for(int j=i*i; j<n; j+=i) {
-                    prime[j] = 1;
+        return answer(n);
+    }
+    public int answer(int n){
+        int[] arr = new int[n];
+        arr[0] = 1;//ye prime nhi hai
+        arr[1] = 1;
+        for(int i=2; i*i < n;i++){
+            if(arr[i] ==0){//prime hai
+                for(int j = i*i; j<n ; j+=i){
+                    arr[j] = 1;
                 }
             }
         }
-
-        int count =0;
-        for(int i=2; i<n; i++) {
-            if(prime[i]==0) {
-                count++;
+        int ans =0;
+        for(int a : arr){
+            if(a==0){
+                ans++;
             }
         }
-
-        return count;
-        
+        return ans;
     }
 }
