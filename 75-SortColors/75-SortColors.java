6082d29466a276be921b5b04550eac8ab779191e
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-26 13:42:27 +0530

    File updated at 9/26/2025, 1:42:27 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,8 +1,34 @@
-// Last updated: 9/26/2025, 1:29:52 PM
+// Last updated: 9/26/2025, 1:42:27 PM
 class Solution {
     public void sortColors(int[] nums) {
-        answer(nums);
+        // answer(nums);
+        answer2(nums);
     }
+
+    public void answer2(int[] nums){
+        int low =0;
+        int mid =0;
+        int high =nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] ==1){
+                mid++;
+            }else{
+                swap(nums, mid , high);
+                high--;
+            }
+        }
+    }
+    public void swap(int[] nums ,int r, int l){
+        int t = nums[r];
+        nums[r] = nums[l];
+        nums[l] = t;
+    }
+
     public void answer(int[] nums){
         int zero =0;
         int one = 0;
