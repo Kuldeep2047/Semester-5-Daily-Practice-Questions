3090a789bb6d7e4660fbee17fb00914f840d991a
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-27 13:36:14 +0530

    File updated at 9/27/2025, 1:36:13 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,34 +1,31 @@
-// Last updated: 9/26/2025, 11:06:17 PM
+// Last updated: 9/27/2025, 1:36:13 PM
 class Solution {
     public int search(int[] nums, int target) {
-        return answer(nums,target);
+        return answer(nums, target);
     }
-    public int answer(int[] arr ,int target){
-        int n = arr.length;
-        int low =0;
-        int high =n-1;
+    public int answer(int[] arr,int target){
+        int low = 0;
+        int high = arr.length-1;
         while(low<=high){
             int mid = (low+high)/2;
-            if(arr[mid]== target){
+            if(arr[mid] == target){
                 return mid;
             }
-            if(arr[low]<=arr[mid]){
-                if(arr[low]<=target && arr[mid]>target){
-                    high = mid-1;
+            if(arr[low] <= arr[mid]){
+                if(arr[low] <= target && target < arr[mid]){
+                    high = mid -1;
                 }else{
                     low = mid+1;
                 }
-            }
-            else{
-                if(arr[mid]<target && arr[high]>=target){
+            }else{
+                if(arr[mid] < target && target <= arr[high]){
                     low = mid+1;
                 }else{
-                    high = mid -1;
+                    high = mid-1;
                 }
 
             }
         }
         return -1;
-
     }
 }
