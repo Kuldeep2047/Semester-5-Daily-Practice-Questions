488a01198e6d69bc228699aadad5418c94e1af1d
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-27 22:15:36 +0530

    File updated at 9/27/2025, 10:15:35 PM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
@@ -1,32 +1,20 @@
-// Last updated: 8/20/2025, 12:18:37 PM
+// Last updated: 9/27/2025, 10:15:35 PM
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        // Arrays.sort(nums);
-        // return(nums[nums.length-k]);
-
-        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        // for(int n:nums){
-        //     pq.add(n);
-        // }
-        // for(int i=1;i<k;i++){
-        //     pq.remove();
-        // }
-        // return pq.peek();
-        return Kth_Largest(nums,k);
-        
+        return answer(nums ,k);
     }
-    public static int Kth_Largest(int[] arr,int k) {
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		for(int i=0;i<k;i++) {
-			pq.add(arr[i]);
-			
-		}
-		for(int i=k;i<arr.length;i++) {
-			if(arr[i]>pq.peek()) {
-				pq.poll();
-				pq.add(arr[i]);
-			}
-		}
-		return pq.peek();
-	}
+    public int answer(int[] arr ,int k){
+        PriorityQueue<Integer> pq= new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            pq.add(arr[i]);
+        }
+        for(int i=k;i<arr.length;i++){
+            if(pq.peek()< arr[i])
+            {
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        return pq.peek();
+    }
 }
