068fe78ea36bdc52a4c68c634377eb42589b5340
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-27 22:31:03 +0530

    File updated at 9/27/2025, 10:31:02 PM

--- 367-ValidPerfectSquare/367-ValidPerfectSquare.java
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.java
@@ -1,26 +1,22 @@
-// Last updated: 8/9/2025, 1:43:45 PM
+// Last updated: 9/27/2025, 10:31:02 PM
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if(num==1||num==4||num==9||num==16||num==25||num==36||num==49) {
-            return true;
-        }
-        int[] validsquare=new int[(int) Math.sqrt(num)];
-        for(int i=0;i<validsquare.length;i++){
-            validsquare[i]=i+1;
-        }
-        int lo=0;
-        int hi=validsquare.length-1;
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            int e=validsquare[mid]*validsquare[mid];
-            if(e==num){
+        return answer(num);
+    }
+
+    public static boolean answer(int n){
+        int low =1;
+        int high = n;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            long square = (long)mid*mid;
+
+            if(square == n){
                 return true;
-            }
-            else if(e<num){
-                lo=mid+1;
-            }
-            else {
-                hi=mid-1;
+            }else if(square<n){
+                low = mid+1;
+            }else{
+                high = mid-1;
             }
         }
         return false;
