096b963df2eaf9c416656f82aef25f8055301994
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-28 15:17:03 +0530

    File updated at 9/28/2025, 3:17:04 PM

--- 162-FindPeakElement/162-FindPeakElement.java
+++ 162-FindPeakElement/162-FindPeakElement.java
@@ -1,20 +1,34 @@
-// Last updated: 9/28/2025, 3:14:59 PM
+// Last updated: 9/28/2025, 3:17:04 PM
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0;
-        int right = nums.length-1;
+        // return answer(nums);
+        return peak_Element(nums);
+    }
+    public int peak_Element(int[] arr){
+        if(arr.length ==1){
+            return 0;
+        }
+        int low=0;
+        int high = arr.length-1;
+        while(low < high){
+            int mid = (low + high)/2;
+            if(arr[mid]<arr[mid+1]){
+                low = mid+1;
+            }else{
+                high = mid;
 
-        while(left<right) {
-            int mid = (left+right)/2;
-
-            if(nums[mid]<nums[mid+1]) {
-                left = mid+1;
-            } else {
-                right = mid;
             }
         }
-
-        return left;
-        
+        return low;
     }
+
+    // public int answer(int[] arr){
+    //     int n = arr.length;
+    //     for(int i=1; i< n-1;i++){
+    //         if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
 }
