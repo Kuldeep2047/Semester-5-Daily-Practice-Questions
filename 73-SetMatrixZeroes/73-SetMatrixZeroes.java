7e69e90776177ccc1bdc1ef8bca7f2d6d9895c26
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-29 14:55:17 +0530

    File updated at 9/29/2025, 2:55:17 PM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.java
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -1,29 +1,58 @@
-// Last updated: 9/29/2025, 2:13:46 PM
+// Last updated: 9/29/2025, 2:55:17 PM
 class Solution {
     public void setZeroes(int[][] matrix) {
-        
-        int m=matrix.length;
-        int n = matrix[0].length;
-        int[] row = new int[m];
-        int[] col = new int[n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+
+        Set<Integer> row = new HashSet<>();
+        Set<Integer> col = new HashSet<>();
+        for(int i=0 ;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    row.add(i);
+                    col.add(j);
                 }
             }
         }
-        assign_zero(matrix,row,col);    
-    }
-    public static int[][] assign_zero(int[][] arr,int[] row,int[] col){
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                if(row[i]==1  || col[j]==1){
-                    arr[i][j]=0;
-                }
-            }
+        for(int r : row){
+            rowChange(matrix, r);
         }
-        return arr;    
+        for(int c : col){
+            colChange(matrix ,c);
+        }
+        
+        // int m=matrix.length;
+        // int n = matrix[0].length;
+        // int[] row = new int[m];
+        // int[] col = new int[n];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]==0){
+        //             row[i]=1;
+        //             col[j]=1;
+        //         }
+        //     }
+        // }
+        // assign_zero(matrix,row,col);    
     }
+
+    public void rowChange(int[][] matrix, int r){
+        for(int c =0;c<matrix[0].length ;c++){
+            matrix[r][c]  =0;
+        }
+    }
+    public void colChange(int[][] matrix, int c){
+        for(int r =0;r<matrix.length ;r++){
+            matrix[r][c]  =0;
+        }
+    }
+
+    // public static int[][] assign_zero(int[][] arr,int[] row,int[] col){
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=0;j<arr[0].length;j++){
+    //             if(row[i]==1  || col[j]==1){
+    //                 arr[i][j]=0;
+    //             }
+    //         }
+    //     }
+    //     return arr;    
+    // }
 }
