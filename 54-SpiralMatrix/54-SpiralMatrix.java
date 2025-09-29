1cfcd6277b46b3e6340e4af1cb4556f3aa990c82
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-29 18:51:07 +0530

    File updated at 9/29/2025, 6:51:06 PM

--- 54-SpiralMatrix/54-SpiralMatrix.java
+++ 54-SpiralMatrix/54-SpiralMatrix.java
@@ -1,40 +1,38 @@
-// Last updated: 8/9/2025, 1:46:02 PM
+// Last updated: 9/29/2025, 6:51:06 PM
 class Solution {
-    public List<Integer> spiralOrder(int[][] arr) {
-        
-		ArrayList<Integer> l = new ArrayList<>();
-		int minr =0, minc=0, maxr = arr.length-1, maxc = arr[0].length-1;
-		int te = (maxr+1) * (maxc+1);
-		int c=0;
-		while(c<te) {
-			for(int i=minc ;i<=maxc && c<te; i++) {
-				l.add(arr[minr][i]);
-				
-				c++;
-			}
-			minr++;
-			for(int i=minr; i<=maxr && c<te; i++) {
-				l.add(arr[i][maxc]);
-				
-				c++;
-			}
-			maxc--;
-			for(int i=maxc;i>=minc && c<te;i--) {
-				l.add(arr[maxr][i]);
-				
-				c++;
-			}
-			maxr--;
-			for(int i=maxr;i>=minr && c<te;i--) {
-				l.add(arr[i][minc]);
-				
-				c++;
-			}
-			minc++;
-		}
-		return l;
-		
-		
-	}
-    
+    public List<Integer> spiralOrder(int[][] matrix) {
+        return Spiral_Print(matrix);
+    }
+    public List<Integer> Spiral_Print(int[][] arr){
+        int minr =0;
+        int maxr = arr.length-1;
+        int minc =0;
+        int maxc = arr[0].length-1;
+        int te = arr.length * arr[0].length;
+        int c =0;
+        List<Integer> ll = new ArrayList<>();
+        while(c<te){
+            for(int i= minc ; i<=maxc && c<te ;i++){
+                ll.add(arr[minr][i]);
+                c++;
+            }
+            minr++;
+            for(int i = minr;i<=maxr && c<te ;i++){
+                ll.add(arr[i][maxc]);
+                c++;
+            }
+            maxc--;
+            for(int i = maxc ;i>=minc && c<te ;i--){
+                ll.add(arr[maxr][i]);
+                c++;
+            }
+            maxr--;
+            for(int i = maxr ; i>=minr && c<te ;i--){
+                ll.add(arr[i][minc]);
+                c++;
+            }
+            minc++;
+        }
+        return ll;
+    }
 }
