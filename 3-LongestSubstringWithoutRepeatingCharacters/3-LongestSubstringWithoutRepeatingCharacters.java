980b1666519dd88de5ee815609aad242f6934254
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-09-30 11:08:06 +0530

    File updated at 9/30/2025, 11:08:05 AM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,24 +1,25 @@
-// Last updated: 8/9/2025, 1:46:51 PM
+// Last updated: 9/30/2025, 11:08:05 AM
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        return Longest_Substring(s);
+        return answer(s);
     }
-    public static int Longest_Substring(String s) {
-		int si=0, ei=0, ans=0;
-		Set<Character> set = new HashSet<>();
-        int len =0;
-        while(ei<s.length()){
-            if(!set.contains(s.charAt(ei))){
-                set.add(s.charAt(ei));
-                ei++;
-                len++;
-                ans = Math.max(ans,len);
-            }else{
-                set.remove(s.charAt(si));
+    public int answer(String s){
+        // Set<Character> set = new HashSet<>();
+        int si=0;
+        int ei =0;
+        // int len =0;
+        int ans =0;
+        int[] arr = new int[255];
+        while(ei < s.length()){
+            while(arr[s.charAt(ei)]>0){
+                arr[s.charAt(si)]--;
                 si++;
-                len--;
             }
+            arr[s.charAt(ei)]++;
+            ans = Math.max(ans, ei-si+1);
+            ei++;
         }
         return ans;
+       
     }
 }
