099256101f01fe2714ffd5e2286ccda637d31aa6
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-01 22:55:53 +0530

    File updated at 10/1/2025, 10:55:52 PM

--- 179-LargestNumber/179-LargestNumber.java
+++ 179-LargestNumber/179-LargestNumber.java
@@ -1,28 +1,32 @@
-// Last updated: 8/9/2025, 1:44:44 PM
+// Last updated: 10/1/2025, 10:55:52 PM
 class Solution {
     public String largestNumber(int[] nums) {
         return largest_num(nums);
     }
     public static String largest_num(int[] nums){
         int n = nums.length;
-        for(int turn =1;turn<n;turn++){
-            for(int i=0;i<n-turn;i++){
-                String num1 = String.valueOf(nums[i]);
-                String num2 = String.valueOf(nums[i + 1]);
-                if ((num2 + num1).compareTo(num1 + num2) > 0){
-                    int temp = nums[i];
-                    nums[i] = nums[i+1];
-                    nums[i+1] = temp;
-                }
+        int f = 0;
+        for(int i=0; i<n; i++) {
+            if(nums[i]!=0) {
+                f = 1;
+                break;
             }
         }
-        StringBuilder s = new StringBuilder();
-        for (int i = 0; i < nums.length; i++) {
-            s.append(nums[i]);
-        }
-        if(s.charAt(0)=='0'){
+        if(f==0) {
             return "0";
         }
-        return s.toString();
+
+        String[] arr = new String[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.toString(nums[i]);
+        }
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+        StringBuilder sb  = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            sb.append(arr[i]);
+        }
+
+        return sb.toString();
     }
 }
