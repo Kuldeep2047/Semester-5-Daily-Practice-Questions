b47da6d7b04380dde7b7c065e296da045f9e4e62
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-01 11:03:24 +0530

    File updated at 10/1/2025, 11:03:23 AM

--- 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.java
+++ 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.java
@@ -1,22 +1,22 @@
-// Last updated: 9/26/2025, 8:41:31 PM
+// Last updated: 10/1/2025, 11:03:23 AM
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        return answer(points);
+        return Burst_Balloons(points);
     }
-    public static int answer(int[][] points){
-        Arrays.sort(points,Comparator.comparingInt(arr->arr[1]));
-        
-        int c=1;
-        int pre = points[0][1];
-        for(int i=1;i<points.length;i++){
-            int ele = points[i][0];
-            if(ele>pre){
+    public int Burst_Balloons(int[][] arr){
+        Arrays.sort(arr , Comparator.comparingInt(a ->a[1]));
+
+        int c =1 ;
+        int first_end = arr[0][1];
+        for(int i=1;i<arr.length ;i++){
+            int second_start = arr[i][0];
+            if(second_start > first_end){
                 c++;
-                pre = points[i][1];
+
+                first_end = arr[i][1];
             }
             
-        }
+        } 
         return c;
-
     }
 }
