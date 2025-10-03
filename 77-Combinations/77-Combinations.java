9f5e40141c1d3c18a123e32c0e3d37371d871352
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-03 20:38:36 +0530

    File updated at 10/3/2025, 8:38:35 PM

--- 77-Combinations/77-Combinations.java
+++ 77-Combinations/77-Combinations.java
@@ -1,20 +1,24 @@
-// Last updated: 10/3/2025, 8:37:04 PM
+// Last updated: 10/3/2025, 8:38:35 PM
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> ll = new ArrayList<>();
-        combination(n,k,1,ans,ll);
+
+        int start =1;
+        combinators(ans, ll, n, k, start);
         return ans;
-        
     }
-    public static void combination(int n,int k,int idx,List<List<Integer>> ans,List<Integer> ll){
-        if(k==0){
+
+    public static void combinators(List<List<Integer>> ans, List<Integer> ll, int n, int k, int start){
+        if(k == 0){
             ans.add(new ArrayList<>(ll));
             return;
         }
-        for(int i=idx;i<=n;i++){
+
+        for(int i = start ;i<=n ;i++){
             ll.add(i);
-            combination(n,k-1,i+1,ans,ll);
+            combinators(ans, ll, n, k-1, i+1);
+
             ll.remove(ll.size()-1);
         }
     }
