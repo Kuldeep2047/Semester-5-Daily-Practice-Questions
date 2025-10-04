93177ec6664af6954d2480cbb392aee799a7c7ef
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-04 22:10:42 +0530

    File updated at 10/4/2025, 10:10:42 PM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,35 +1,23 @@
-// Last updated: 9/3/2025, 11:53:52 AM
+// Last updated: 10/4/2025, 10:10:42 PM
 class Solution {
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        // return Robber(nums,0,dp);
-        return RobberBU(nums);
-        
+        Arrays.fill(dp ,-1);
+
+        return Max_Rob(nums, 0 , dp);
     }
-    public static int RobberBU(int[] arr) {
-		if(arr.length == 1) {
-			return arr[0];
-		}
-		int[] dp = new int[arr.length];
-		dp[0] = arr[0];
-		dp[1] = Math.max(arr[0],arr[1] );
-		for(int i=2;i<dp.length;i++) {
-			int rob = arr[i] + dp[i-2];
-			int Dont_rob = dp[i-1];
-			dp[i] = Math.max(rob, Dont_rob);
-		}
-		return dp[dp.length - 1];
-	}
-    public int Robber(int[] nums,int i,int[] dp){
-        if(i>=nums.length){
+
+    public static int Max_Rob(int[] arr, int idx, int[] dp){
+        if(idx >= arr.length){
             return 0;
         }
-        if(dp[i] != -1){
-            return dp[i];
+
+        if(dp[idx] != -1){
+            return dp[idx];
         }
-        int rob = nums[i] + Robber(nums, i+2,dp);
-        int not_rob = Robber(nums,i+1, dp);
-        return dp[i] = Math.max(rob, not_rob);
+
+        int not_rob = Max_Rob(arr, idx +1, dp);
+        int rob = arr[idx] + Max_Rob(arr, idx +2, dp);
+        return dp[idx] = Math.max(not_rob, rob);
     }
 }
