c957569a003388547481e1f4a1bb31ace15ca0ea
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-04 15:15:43 +0530

    File updated at 10/4/2025, 3:15:42 PM

--- 36-ValidSudoku/36-ValidSudoku.java
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -1,50 +1,43 @@
-// Last updated: 9/1/2025, 12:08:14 PM
+// Last updated: 10/4/2025, 3:15:42 PM
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        return isValid(board);
-    }
-    public boolean isValid(char[][] board){
-        //check each row
-        for(int i=0;i<9;i++){
-            HashMap<Character,Integer> map=new HashMap<>();
-            for(int j=0;j<9;j++){
-                if(board[i][j] != '.'){
-                    if(map.containsKey(board[i][j])){
-                        return false;
-                    }else{
-                        map.put(board[i][j],1);
-                    }
-                }
-            }
-        }
-
-        for(int i=0;i<9;i++){
-            HashMap<Character,Integer> map = new HashMap<>();
-            for(int j=0;j<9;j++){
-                if(board[j][i] != '.'){
-                    if(map.containsKey(board[j][i])){
-                        return false;
-                    }else{
-                        map.put(board[j][i],1);
-                    }
-                }
-            }
-        }
-
-        for(int k = 0; k < 9; k++) {
-            HashSet<Character> set = new HashSet<>();
-            int startRow = 3 * (k / 3);
-            int startCol = 3 * (k % 3);
-            for(int i = 0; i < 3; i++) {
-                for(int j = 0; j < 3; j++) {
-                    char ch = board[startRow + i][startCol + j];
-                    if(ch != '.') {
-                        if(set.contains(ch)) return false;
-                            set.add(ch);
-                    }
+        for(int i =0 ;i<board.length ;i++){
+            for(int  j=0 ;j<board[0].length ;j++){
+                if(board[i][j] == '.'){
+                    continue;
+                }if(! isitsafe(board, i, j, board[i][j]))
+                {
+                    return false;
                 }
             }
         }
         return true;
     }
+
+    public static boolean isitsafe(char[][] grid,int row,int col,char val) {
+			//row
+			for(int i=0;i<9;i++) {
+				if(grid[row][i]==val && i!= col) {
+					return false;
+				}
+			}
+			//col
+			for(int i=0;i<9;i++) {
+				if(grid[i][col]==val && i!=row) {
+					return false;
+				}
+			}
+			// 3x3 Matrix
+			int r = row-row % 3;
+			int c = col-col % 3;
+			for(int i=r;i<r+3;i++) {
+				for(int j=c;j<c+3;j++) {
+					if(grid[i][j]==val && (i != row || j != col)) {
+						return false;
+					}
+				}
+			}
+			return true;
+			
+		}
 }
