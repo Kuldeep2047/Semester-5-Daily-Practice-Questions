2e552ac7942e435e267f6abdc417340f9a1fad00
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-04 14:39:33 +0530

    File updated at 10/4/2025, 2:39:32 PM

--- 37-SudokuSolver/37-SudokuSolver.java
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -1,59 +1,59 @@
-// Last updated: 8/9/2025, 1:46:23 PM
+// Last updated: 10/4/2025, 2:39:32 PM
 class Solution {
     public void solveSudoku(char[][] board) {
-        Print(board,0,0);
+        fill_Suduko(board, 0, 0);
     }
-    public static boolean Print(char[][] grid,int row,int col) {
-			if(col==9) {
-				row++;
-				col=0;
-			}
-			if(row==9) {
-				return true;
-			}
-			
-			if(grid[row][col] != '.') {
-				return Print(grid,row,col+1);
-			}else {
-				for(int val=1; val<=9; val++) {
-					if(isitsafe(grid,row,col,(char)(val+'0'))) {
-						grid[row][col] =(char)(val+'0');
-						boolean ans = Print(grid,row,col+1);
-						if(ans) {
-							return true;
-						}
-						grid[row][col]='.';
-					}
-				}
-			}
-			return false;
-			
-		}
-		
-		public static boolean isitsafe(char[][] grid,int row,int col,char val) {
-			//row
-			for(int i=0;i<9;i++) {
-				if(grid[row][i]==val) {
-					return false;
-				}
-			}
-			//col
-			for(int i=0;i<9;i++) {
-				if(grid[i][col]==val) {
-					return false;
-				}
-			}
-			// 3x3 Matrix
-			int r = row-row % 3;
-			int c = col-col % 3;
-			for(int i=r;i<r+3;i++) {
-				for(int j=c;j<c+3;j++) {
-					if(grid[i][j]==val) {
-						return false;
-					}
-				}
-			}
-			return true;
-			
-		}
+
+    public boolean fill_Suduko(char[][] board, int row ,int col){
+        if(col ==9){
+            row++;
+            col =0;
+        }
+        if(row == 9){
+            return true;
+        }
+        if(board[row][col] != '.'){
+            return fill_Suduko(board, row ,col+1);
+        }else{
+            for(int val =1 ;val<=9;val++){
+                if(isitsafe(board, row, col, val)){
+                    board[row][col] = (char)(val+'0');
+                    boolean ans = fill_Suduko(board, row, col+1);
+                    if(ans){
+                        return true;
+                    }
+                    board[row][col] = '.';
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isitsafe(char[][] board, int row, int col, int val){
+        //row
+        char ch = (char)(val+'0');
+        for(int i =0 ;i<board.length ;i++){
+            if(board[i][col] == ch){
+                return false;
+            }
+        }
+
+        for(int i =0 ;i<board[0].length ;i++){
+            if(board[row][i] == ch){
+                return false;
+            }
+        }
+
+        int r = row - row%3;
+        int c = col - col%3;
+        for(int i= r ;i< r+3 ;i++){
+            for(int j = c ;j< c+3 ;j++){
+                if(board[i][j] == ch){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
 }
