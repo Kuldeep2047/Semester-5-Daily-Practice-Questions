59f49f2b96f490d23fac0d63abe20226cfdc9ad1
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-04 11:29:12 +0530

    File updated at 10/4/2025, 11:29:12 AM

--- 131-PalindromePartitioning/131-PalindromePartitioning.java
+++ 131-PalindromePartitioning/131-PalindromePartitioning.java
@@ -1,38 +1,39 @@
-// Last updated: 8/9/2025, 1:45:16 PM
+// Last updated: 10/4/2025, 11:29:12 AM
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
-		List<String> ll = new ArrayList<>();
-		Partitioning(s,ll,ans);
-		return ans;
+        List<String> ll = new ArrayList<>();
+        Palindrome_Partition(s, ll ,ans);
+        return ans;
     }
-    public static void Partitioning(String ques,List<String> ll,List<List<String>> ans) {
-		if(ques.length()==0) {
-//			System.out.println(ans);
-			ans.add(new ArrayList<>(ll));
-			return;
-		}
-		
-		for(int cut=1;cut<=ques.length();cut++) {
-			String s = ques.substring(0,cut);
-			
-			if(isPalindrome(s)) {
-				ll.add(s);
-				Partitioning(ques.substring(cut),ll,ans);
-				ll.remove(ll.size()-1);
-			}
-		}
-	}
-	public static boolean isPalindrome(String s) {
-		int i=0;
-		int j=s.length()-1;
-		while(i<j) {
-			if(s.charAt(i) != s.charAt(j)) {
-				return false;
-			}
-			i++;
-			j--;
-		}
-		return true;
-	}
+
+    public static void Palindrome_Partition(String ques, List<String> ll,List<List<String>> ans){
+
+            if(ques.length() ==0 ){
+                ans.add(new ArrayList<>(ll));
+                return;
+            }
+
+            for(int cut =1 ;cut<=ques.length() ;cut++){
+                String part = ques.substring(0,cut);
+                if(isPalindrome(part)){
+                    ll.add(part);
+                    Palindrome_Partition(ques.substring(cut), ll, ans);
+                    ll.remove(ll.size()-1);
+                }
+            }
+    }
+
+    public static boolean isPalindrome(String s){
+        int i =0;
+        int j =s.length()-1;
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
