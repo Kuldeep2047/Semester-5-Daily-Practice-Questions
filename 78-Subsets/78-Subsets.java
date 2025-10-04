d30833df59466ef96aa0c0fadc392830abba06da
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-04 10:56:32 +0530

    File updated at 10/4/2025, 10:56:31 AM

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,4 +1,4 @@
-// Last updated: 10/4/2025, 10:52:32 AM
+// Last updated: 10/4/2025, 10:56:31 AM
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -7,19 +7,33 @@ class Solution {
         Sub_Sets(nums, 0, ll, ans);
         return ans;
     }
-
     public static void Sub_Sets(int[] nums , int idx, List<Integer> ll, List<List<Integer>> ans){
-        if(idx == nums.length){
-            ans.add(new ArrayList<>(ll));
-            return;
-        }
+        
+        ans.add(new ArrayList<>(ll));  // in last add in ans list
+        
 
-        ll.add(nums[idx]);
-        Sub_Sets(nums, idx+1, ll, ans);
-        ll.remove(ll.size()-1);
-        Sub_Sets(nums, idx+1, ll, ans);
+        for(int i = idx ;i<nums.length ;i++){
+            ll.add(nums[i]);
+            Sub_Sets(nums, i+1, ll, ans);
+            ll.remove(ll.size()-1);
+        }
 
 
 
     }
+
+    // public static void Sub_Sets(int[] nums , int idx, List<Integer> ll, List<List<Integer>> ans){
+    //     if(idx == nums.length){
+    //         ans.add(new ArrayList<>(ll));
+    //         return;
+    //     }
+
+    //     ll.add(nums[idx]);
+    //     Sub_Sets(nums, idx+1, ll, ans);
+    //     ll.remove(ll.size()-1);
+    //     Sub_Sets(nums, idx+1, ll, ans);
+
+
+
+    // }
 }
