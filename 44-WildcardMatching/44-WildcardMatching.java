df89cf130aac1cb3f7bca311bdc37a3d5f698290
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-05 17:58:40 +0530

    Create 44-WildcardMatching

---
+++ 44-WildcardMatching/44-WildcardMatching.java
@@ -0,0 +1,59 @@
+// Last updated: 10/5/2025, 5:58:39 PM
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int[][] dp = new int[s.length()][p.length()];
+        for(int[] ar : dp){
+            Arrays.fill(ar, -1);
+        }
+
+        return Wildcard_Matching(s, p, s.length()-1, p.length()-1, dp);
+    }
+
+    public boolean Wildcard_Matching(String s ,String p , int i ,int j, int[][] dp){
+        //base case
+        if(i < 0  && j < 0){
+            return true;
+        }
+        if(i>=0 && j<0){
+            return false;
+        }
+        if(i<0 && j>=0){
+            for(int k = 0 ;k<=j; k++){
+                if(p.charAt(k) != '*'){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        if(dp[i][j] != -1){
+            if(dp[i][j] == 1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        //logic
+        boolean match;
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
+            match = Wildcard_Matching(s, p, i-1 ,j-1, dp);
+        }
+        else if(p.charAt(j)== '*'){
+            boolean a = Wildcard_Matching(s, p, i-1, j, dp);
+            boolean b = Wildcard_Matching(s, p, i ,j-1, dp);
+            match = a || b;
+        }else{
+            match = false;
+        }
+
+        
+       if (match == true) {
+            dp[i][j] = 1;
+        } else {
+            dp[i][j] = 0;
+        }
+
+        return match;
+    }
+}
