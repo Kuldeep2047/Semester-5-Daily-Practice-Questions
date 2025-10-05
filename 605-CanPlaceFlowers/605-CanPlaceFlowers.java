d86750cf263bc8715479f7e01e47f03cf054e529
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-05 14:19:59 +0530

    File updated at 10/5/2025, 2:19:58 PM

--- 605-CanPlaceFlowers/605-CanPlaceFlowers.java
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.java
@@ -1,16 +1,29 @@
-// Last updated: 8/9/2025, 1:42:45 PM
+// Last updated: 10/5/2025, 2:19:58 PM
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if (n == 0) {
+        return answer(flowerbed , n);
+    }
+
+    public static boolean answer(int[] bed, int n){
+        if(n==0){
             return true;
         }
-        for (int i = 0; i < flowerbed.length; i++) {
-            if ((i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0) && flowerbed[i] == 0) {
-                flowerbed[i] = 1;
-                n--;
-                if (n == 0) {
+        
+        for(int  i= 0;i<bed.length ;i++){
+            int v = bed[i];
+            if(v == 0){
+
+                boolean left = (i == 0) || (bed[i - 1] == 0);
+                boolean right = (i == bed.length - 1) || (bed[i + 1] == 0);
+
+                if(left && right){
+                    bed[i] = 1;
+                    n--;
+                }
+                if(n == 0){
                     return true;
                 }
+
             }
         }
         return false;
