7f2ebf403e0ce171f732da2234883d45d877446b
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-06 14:25:45 +0530

    File updated at 10/6/2025, 2:25:44 PM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
@@ -1,28 +1,26 @@
-// Last updated: 8/9/2025, 1:42:30 PM
+// Last updated: 10/6/2025, 2:25:44 PM
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        return Product_Less_Than_K(nums,k);
+        return product(nums ,k);
     }
-    public static int Product_Less_Than_K(int[] arr,int k) {
-		int ans=0;
-		int si=0;
-		int ei=0;
-		int p=1;
-		while(ei<arr.length) {
-			//grow
-			p=p*arr[ei];
-			
-			//shrink
-			while(p>=k && si<=ei) {
-				p=p/arr[si];
-				si++;
-			}
-			
-			//ans update
-			ans = ans+(ei-si+1) ;
-			ei++;
-		}
-		return ans;
-		
-	}
+    public int product(int[] arr ,int k){
+        int n = arr.length;
+        int si =0;
+        int ei =0;
+        int p=1;
+        int ans =0;
+
+        while(ei < n){
+            p *= arr[ei];
+
+            while(p>=k && si<=ei){
+                p =p/arr[si];
+                si++;
+            }
+
+            ans += (ei-si+1);
+            ei++;
+        }
+        return ans;
+    }
 }
