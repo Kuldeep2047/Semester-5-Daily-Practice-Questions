872d744f1cfc3aad652939e525f990d511c45b60
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-06 11:26:01 +0530

    File updated at 10/6/2025, 11:26:00 AM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.java
@@ -1,21 +1,21 @@
-// Last updated: 9/2/2025, 4:15:18 PM
+// Last updated: 10/6/2025, 11:26:00 AM
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int[] dp = new int[cost.length];
-        Arrays.fill(dp,-1);
-        int zero = answer(cost,0,dp);
-        int first = answer(cost,1,dp);
-        return Math.min(zero,first);
+        Arrays.fill(dp ,-1);
+        int zero = helper(cost, dp, 0);
+        int one = helper(cost, dp ,1);
+        return Math.min(one , zero);
     }
-    public int answer(int[] arr,int idx, int[] dp){
-        if(idx>=arr.length){
+    public int helper(int[] cost, int[] dp ,int idx){
+        if(idx >= cost.length){
             return 0;
         }
         if(dp[idx] != -1){
             return dp[idx];
         }
-        int jump1 = answer(arr,idx+1,dp);
-        int jump2 = answer(arr,idx+2,dp);
-        return dp[idx] = arr[idx] + Math.min(jump1,jump2);
+        int one = cost[idx] + helper(cost, dp ,idx+1);
+        int two = cost[idx] + helper(cost, dp ,idx+2);
+        return dp[idx] = Math.min(one ,two);
     }
 }
