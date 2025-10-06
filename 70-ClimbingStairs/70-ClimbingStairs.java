6d49dd52883799c1195a069d8784d2bcbcb015d1
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-06 11:09:28 +0530

    File updated at 10/6/2025, 11:09:27 AM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,22 +1,24 @@
-// Last updated: 8/31/2025, 12:59:29 PM
+// Last updated: 10/6/2025, 11:09:27 AM
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return answer(n, 0, dp);
+        int[] dp = new int[n];
+        Arrays.fill(dp ,-1);
+
+        return answer(n, dp, 0);
     }
-    public int answer(int n, int idx, int[] dp){
+    public int answer(int n, int[] dp ,int idx){
         if(idx == n){
             return 1;
         }
-        if(idx>n){
+        if(idx > n){
             return 0;
         }
         if(dp[idx] != -1){
             return dp[idx];
         }
-        int one = answer(n, idx+1, dp);
-        int two = answer(n, idx+2, dp);
-        return dp[idx] = one +two;
+        int a = answer(n , dp ,idx+1);
+        int b = answer(n, dp, idx+2);
+        return dp[idx] = a+b;
+
     }
 }
