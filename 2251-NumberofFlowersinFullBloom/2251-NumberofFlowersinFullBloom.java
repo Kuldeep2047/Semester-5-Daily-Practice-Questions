16a7c1308abeb9f6c1f996c02f5bff57aeceda4f
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-13 18:35:59 +0530

    File updated at 10/13/2025, 6:35:59 PM

--- 2251-NumberofFlowersinFullBloom/2251-NumberofFlowersinFullBloom.java
+++ 2251-NumberofFlowersinFullBloom/2251-NumberofFlowersinFullBloom.java
@@ -1,14 +1,16 @@
-// Last updated: 10/13/2025, 6:35:15 PM
+// Last updated: 10/13/2025, 6:35:59 PM
 class Solution {
     public int[] fullBloomFlowers(int[][] flowers, int[] people) {
-        return numberOfFlowers(flowers, people);
+        return answer(flowers, people);
     }
-    public int[] numberOfFlowers(int[][] flowers, int[] people){
-        int n = flowers.length;
-        int[] start = new int[n];
-        int[] end = new int[n];
+    public int[] answer(int[][] flowers, int[] people){
+        int n = people.length;
+        int[] ans = new int[n];
 
-        for(int i =0 ;i<n;i++){
+        int[] start = new int[flowers.length];
+        int[] end = new int[flowers.length];
+
+        for(int i=0; i<flowers.length ;i++){
             start[i] = flowers[i][0];
             end[i] = flowers[i][1];
         }
@@ -16,39 +18,39 @@ class Solution {
         Arrays.sort(start);
         Arrays.sort(end);
 
-        int[] ans = new int[people.length];
-
-        for(int i =0 ;i< people.length ;i++){
-            int before_start = upperBound(start, people[i]); // flower bloom count started ≤ person time
-            int before_end = lowerBound(end, people[i]);    //count of flowers ended < person time
-
-            ans[i] = before_start - before_end;
+        for(int i= 0;i<people.length ;i++){
+            int a = upperbound(start, people[i]);
+            int b = lowerbound(end, people[i]);
+            ans[i] = a-b;
         }
         return ans;
     }
-
-    public static int upperBound(int[] arr, int item) {
-        
-        int low = 0, high = arr.length;
-        while (low < high) {
-            int mid = low + (high-low)/2;
-            if (arr[mid] <= item)
-                low = mid + 1;
-            else
-                high = mid;
+    public int upperbound(int[] arr , int tar){
+        int n = arr.length;
+        int si =0;
+        int ei = n;
+        while(si<ei){
+            int mid = (si + ei)/2;
+            if(arr[mid]<=tar){
+                si = mid +1;
+            }else{
+                ei = mid;
+            }
         }
-        return low;
+        return si;
     }
-
-    public static int lowerBound(int[] arr, int item) {
-        int low = 0, high = arr.length;
-        while (low < high) {
-            int mid = low + (high-low)/2;
-            if (arr[mid] < item)
-                low = mid + 1;
-            else
-                high = mid;
+    public int lowerbound(int[] arr , int tar){
+        int n = arr.length;
+        int si =0;
+        int ei = n;
+        while(si<ei){
+            int mid = (si + ei)/2;
+            if(arr[mid]<tar){
+                si = mid +1;
+            }else{
+                ei = mid;
+            }
         }
-        return low;
+        return si;
     }
 }
