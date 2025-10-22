7a9427ca7c8ff655641f2bbed4b6a9e737e1fbb5
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-22 21:43:01 +0530

    File updated at 10/22/2025, 9:43:00 PM

--- 394-DecodeString/394-DecodeString.java
+++ 394-DecodeString/394-DecodeString.java
@@ -1,45 +1,43 @@
-// Last updated: 8/9/2025, 1:43:30 PM
+// Last updated: 10/22/2025, 9:43:00 PM
 class Solution {
     public String decodeString(String s) {
-        return answer(s);
+        return decode(s);
     }
-
-    public static String answer(String s){
-
+    public static String decode(String s){
+        int n = s.length();
+        Stack<Integer> stNum = new Stack<>();
         Stack<String> st = new Stack<>();
-        int num = 0;
 
-        for(int i = 0; i < s.length(); i++){
+        int currNum = 0;
+        String currStr = "";
+
+        for(int i=0 ;i<n ; i++){
             char ch = s.charAt(i);
 
-            if(Character.isDigit(ch)){
+            if(ch == '['){
+                stNum.push(currNum);
+                st.push(currStr);
+                currStr = "";
+                currNum = 0;
+            }
+            else if(ch == ']'){
+                String prevStr = st.pop();
+                int r = stNum.pop();
 
-                num = num * 10 + (ch - '0');
-            } else if(ch =='['){
-                st.push(String.valueOf(num));
-                st.push("[");
-                num = 0;
-
-            } else if(ch == ']'){
-
-                StringBuilder sb = new StringBuilder();
-                while(!st.peek().equals("[")) {
-                    sb.insert(0, st.pop());
+                StringBuilder sb = new StringBuilder(prevStr);
+                for(int k=0; k<r ;k++){
+                    sb.append(currStr);
                 }
-                
-                st.pop();
-                int count = Integer.parseInt(st.pop());
-                String repeated = sb.toString().repeat(count);
-                st.push(repeated);
-            } else {
-                st.push(String.valueOf(ch)); 
+
+                currStr = sb.toString();
+            }
+            else if(ch>='0' && ch<='9'){
+                currNum = currNum*10 + (ch-'0');
+            }
+            else{
+                currStr += ch;
             }
         }
-        StringBuilder ans = new StringBuilder();
-        for(String str : st){
-            ans.append(str);
-        }
-
-        return ans.toString();
+        return currStr;
     }
 }
