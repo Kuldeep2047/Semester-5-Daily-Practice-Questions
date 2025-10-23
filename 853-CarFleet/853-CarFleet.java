e38049ab7a3788d7f7a9adbe77ee94eea1c5eaa1
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-10-23 21:05:20 +0530

    Create 853-CarFleet

---
+++ 853-CarFleet/853-CarFleet.java
@@ -0,0 +1,25 @@
+// Last updated: 10/23/2025, 9:05:19 PM
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n= position.length;
+         
+        double[][]cars= new double[n][2];
+
+        for(int i=0 ;i<n ;i++){
+            cars[i][0]= position[i];
+            cars[i][1]= (double)(target-position[i])/speed[i];
+        }
+         
+        Arrays.sort(cars, (a,b)->Double.compare(b[0],a[0]));
+        Stack<Double>stack= new Stack<>();
+
+        for(double[]car:cars){
+            double time= car[1];
+        
+            while(stack.isEmpty()|| time>stack.peek()){
+                stack.push(time);
+            }
+        }
+        return stack.size();
+    }
+}
