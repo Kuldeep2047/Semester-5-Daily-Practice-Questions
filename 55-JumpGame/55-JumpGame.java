0cbc6494d77d273b1f97def0d25a0eae7ade1367
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-11-03 20:45:42 +0530

    File updated at 11/3/2025, 8:45:42 PM

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,30 +1,30 @@
-// Last updated: 10/1/2025, 2:16:17 PM
+// Last updated: 11/3/2025, 8:45:42 PM
 class Solution {
     public boolean canJump(int[] nums) {
         return answer(nums);
     }
     public static boolean answer(int[] arr){
         int n = arr.length;
-        // int tar = n-1;
-        // for(int i=n-1;i>=0;i--){
-        //     if(arr[i]+i >= tar){
-        //         tar = i;
-        //     }
-        // }
-        // if(tar==0){
-        //     return true;
-        // }else{
-        //     return false;
-        // }
-
-        int maxReach = 0;
-        for(int i=0; i<n ;i++){
-            if(i>maxReach){
-                return false;
+        int tar = n-1;
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]+i >= tar){
+                tar = i;
             }
-            maxReach = Math.max(maxReach, arr[i] + i);
+        }
+        if(tar==0){
+            return true;
+        }else{
+            return false;
         }
 
-        return true;
+        // int maxReach = 0;
+        // for(int i=0; i<n ;i++){
+        //     if(i>maxReach){
+        //         return false;
+        //     }
+        //     maxReach = Math.max(maxReach, arr[i] + i);
+        // }
+
+        // return true;
     }
 }
