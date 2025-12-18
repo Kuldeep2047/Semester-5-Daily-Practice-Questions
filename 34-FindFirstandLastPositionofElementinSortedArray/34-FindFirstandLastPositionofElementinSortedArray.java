d7bafae4347bbc15862b02596b5c7dc8b6695d0b
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-12-18 22:25:49 +0530

    File updated at 12/18/2025, 10:25:49 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
@@ -1,43 +1,26 @@
-// Last updated: 9/26/2025, 8:37:00 PM
-class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] arr = {-1,-1};
-        arr[0]= first_search(nums,target);
-        arr[1]= last_search(nums,target);
-        return arr;
-    }
-    public static int first_search(int[] arr,int target){
-        int start=0;
-        int end = arr.length-1;
-        int first=-1;
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(arr[mid]==target){
-                first=mid;
-                end=mid-1;
-            }else if(arr[mid]<target){
-                start=mid+1;
-            }else{
-                end=mid-1;
-            }
-        }
-        return first;
-    }
-    public static int last_search(int[] arr,int target){
-        int start=0;
-        int end = arr.length-1;
-        int sec=-1;
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(arr[mid]==target){
-                sec=mid;
-                start=mid+1;
-            }else if(arr[mid]<target){
-                start=mid+1;
-            }else{
-                end=mid-1;
-            }
-        }
-        return sec;
-    }
-}
+// Last updated: 12/18/2025, 10:25:49 PM
+1class Solution {
+2    public int[] searchRange(int[] nums, int target) {
+3        return answer(nums, target);
+4    }
+5
+6    public int[] answer(int[] arr ,int tar){
+7        int n = arr.length;
+8        int[] ans = {-1,-1};
+9
+10        for(int i=0 ;i<n ;i++){
+11            if(arr[i] == tar){
+12                ans[0] = i;
+13                break;
+14            }
+15        }
+16
+17        for(int i=n-1 ;i>=0 ;i--){
+18            if(arr[i] == tar){
+19                ans[1] = i;
+20                break;
+21            }
+22        }
+23        return ans;
+24    }
+25}
