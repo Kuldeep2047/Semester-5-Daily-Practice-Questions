14b6f0ffcdc7825da94e9e8a3d0d161080df0749
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-12-18 22:10:31 +0530

    File updated at 12/18/2025, 10:10:31 PM

--- 63-UniquePathsII/63-UniquePathsII.java
+++ 63-UniquePathsII/63-UniquePathsII.java
@@ -1,31 +1,31 @@
-// Last updated: 10/4/2025, 9:39:40 PM
-class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-
-        int[][] dp = new int[m][n];
-        return paths(0,0 ,m-1,n-1,dp,obstacleGrid);
-    }
-
-    public static int paths(int cr, int cc, int er ,int ec, int[][] dp, int[][] obstacle){
-        if(cr > er || cc > ec || obstacle[cr][cc] == 1){
-            return 0;
-        }
-        if(cr == er && cc == ec){
-            return 1;
-        }
-
-        
-
-        if(dp[cr][cc] != 0){
-            return dp[cr][cc];
-        }
-        
-        int a = paths(cr +1, cc, er, ec, dp, obstacle);
-        int b = paths(cr , cc+1, er, ec, dp, obstacle);
-
-        return dp[cr][cc] = a+b;
-        
-    }
-}
+// Last updated: 12/18/2025, 10:10:31 PM
+1class Solution {
+2    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+3        int m = obstacleGrid.length;
+4        int n = obstacleGrid[0].length;
+5
+6        int[][] dp = new int[m][n];
+7        return paths(0,0 ,m-1,n-1,dp,obstacleGrid);
+8    }
+9
+10    public static int paths(int cr, int cc, int er ,int ec, int[][] dp, int[][] obstacle){
+11        if(cr > er || cc > ec || obstacle[cr][cc] == 1){
+12            return 0;
+13        }
+14        if(cr == er && cc == ec){
+15            return 1;
+16        }
+17
+18        
+19
+20        if(dp[cr][cc] != 0){
+21            return dp[cr][cc];
+22        }
+23        
+24        int a = paths(cr +1, cc, er, ec, dp, obstacle);
+25        int b = paths(cr , cc+1, er, ec, dp, obstacle);
+26
+27        return dp[cr][cc] = a+b;
+28        
+29    }
+30}
