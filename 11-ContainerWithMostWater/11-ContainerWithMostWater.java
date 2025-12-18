72c562b2c8fbb8ff72a8fa3f1476482f4e15e737
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-12-18 22:33:27 +0530

    File updated at 12/18/2025, 10:33:28 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,25 +1,27 @@
-// Last updated: 11/15/2025, 10:54:34 PM
-class Solution {
-    public int maxArea(int[] height) {
-        return Most_water(height);
-        
-    }
-    public static int Most_water(int[] arr){
-        int left=0;
-        int right=arr.length-1;
-        int ans=0;
-        while(left<right){
-            int width = right-left;
-
-            int height = Math.min(arr[right],arr[left]);
-            ans=Math.max(ans, width*height);
-            
-            if(arr[left]<arr[right]){
-                left++;
-            }else{
-                right--;
-            }
-        }
-        return ans;
-    }
-}
+// Last updated: 12/18/2025, 10:33:28 PM
+1class Solution {
+2    public int maxArea(int[] height) {
+3        return answer(height);
+4    }
+5
+6    public int answer(int[] arr){
+7        int n = arr.length;
+8        int left =0;
+9        int right = n-1;
+10
+11        int ans = Integer.MIN_VALUE;
+12
+13        while(left < right){
+14            int w = right - left;
+15            int h = Math.min(arr[left], arr[right]);
+16            ans = Math.max(ans, w*h);
+17
+18            if(arr[right] < arr[left]){
+19                right--;
+20            }else{
+21                left++;
+22            }
+23        }
+24        return ans;
+25    }
+26}
