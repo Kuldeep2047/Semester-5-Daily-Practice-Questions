c13a093c21dac1a7ea0eb2a33c5c0b773e11d979
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-12-18 23:01:14 +0530

    File updated at 12/18/2025, 11:01:15 PM

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,23 +1,24 @@
-// Last updated: 10/6/2025, 10:23:02 AM
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-		List<Integer> ll = new ArrayList<>();
-		Combination(candidates,target,ll,0,ans);
-		return ans;
-        
-    }
-    public static void Combination(int[] coin, int amount,List<Integer> ll,int idx,List<List<Integer>> ans) {
-		if(amount==0) {
-			ans.add(new ArrayList<>(ll));
-			return;
-		}
-		for(int i=idx;i<coin.length;i++) {
-			if(amount>=coin[i]) {
-				ll.add(coin[i]);
-				Combination(coin,amount-coin[i],ll,i,ans);
-				ll.remove(ll.size()-1);
-			}
-		}
-	}
-}
+// Last updated: 12/18/2025, 11:01:15 PM
+1class Solution {
+2    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+3        List<List<Integer>> ans = new ArrayList<>();
+4		List<Integer> ll = new ArrayList<>();
+5		Combination(candidates,target,ll,0,ans);
+6		return ans;
+7        
+8    }
+9    public static void Combination(int[] coin, int amount,List<Integer> ll,int idx,List<List<Integer>> ans) {
+10		if(amount==0) {
+11			ans.add(new ArrayList<>(ll));
+12			return;
+13		}
+14
+15		for(int i=idx;i<coin.length;i++) {
+16			if(amount>=coin[i]) {
+17				ll.add(coin[i]);
+18				Combination(coin,amount-coin[i],ll,i,ans);
+19				ll.remove(ll.size()-1);
+20			}
+21		}
+22	}
+23}
