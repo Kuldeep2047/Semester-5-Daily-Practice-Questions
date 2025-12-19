22ad8cd01a87dee3b43f186f4b527caf8821fa3d
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-12-19 17:59:14 +0530

    File updated at 12/19/2025, 5:59:15 PM

--- 720-LongestWordinDictionary/720-LongestWordinDictionary.java
+++ 720-LongestWordinDictionary/720-LongestWordinDictionary.java
@@ -1,36 +1,36 @@
-// Last updated: 8/31/2025, 10:58:06 PM
-class Solution {
-    public String longestWord(String[] words) {
-        return answer(words);
-    }
-    public String answer(String[] arr){
-        int n = arr.length;
-        Arrays.sort(arr, (a,b)->{
-            if(a.length() != b.length()){
-                return b.length()-a.length();
-            }else{
-                return a.compareTo(b);
-            }
-        });
-
-        Set<String> set = new HashSet<>();
-        for(String s : arr){
-            set.add(s);
-        }
-
-        for(String word : arr){
-            boolean ans = true;
-            for(int i=1;i<word.length();i++){
-                if(! set.contains(word.substring(0,i))){
-                    ans = false;
-
-                    break;
-                }
-            }
-            if(ans== true){
-                return word;
-            }
-        }
-        return "";
-    }
-}
+// Last updated: 12/19/2025, 5:59:15 PM
+1class Solution {
+2    public String longestWord(String[] words) {
+3        return answer(words);
+4    }
+5    public String answer(String[] arr){
+6        int n = arr.length;
+7        Arrays.sort(arr, (a,b)->{
+8            if(a.length() != b.length()){
+9                return b.length()-a.length();
+10            }else{
+11                return a.compareTo(b);
+12            }
+13        });
+14
+15        Set<String> set = new HashSet<>();
+16        for(String s : arr){
+17            set.add(s);
+18        }
+19
+20        for(String word : arr){
+21            boolean ans = true;
+22            for(int i=1;i<word.length();i++){
+23                if(! set.contains(word.substring(0,i))){
+24                    ans = false;
+25
+26                    break;
+27                }
+28            }
+29            if(ans== true){
+30                return word;
+31            }
+32        }
+33        return "";
+34    }
+35}
