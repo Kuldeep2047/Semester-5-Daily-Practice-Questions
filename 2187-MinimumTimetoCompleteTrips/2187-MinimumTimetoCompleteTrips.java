b461071b121d8674373fed0dae2ef6118b293ea9
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-12-26 12:11:42 +0530

    File updated at 12/26/2025, 12:11:43 PM

--- 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
+++ 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
@@ -1,36 +1,37 @@
-// Last updated: 10/5/2025, 2:03:29 PM
-class Solution {
-    public long minimumTime(int[] time, int totalTrips) {
-        return answer(time, totalTrips);
-    }
-    public long answer(int[] time, int tT){
-        Arrays.sort(time);
-        long low = time[0];
-        long high = (long)tT * time[0];
-        
-        long ans = high;
-
-        while(low<=high){
-            long mid = low - (low-high)/2;
-            if(isPossible(time, mid, tT)){
-                ans = mid;
-                high = mid-1;
-            }else{
-                low = mid+1;
-            }
-        }
-        return ans;
-    }
-
-    public boolean isPossible(int[] time ,long mid, int tT){
-        long trip = 0;
-        for(int t : time){
-            trip += mid/t;
-            if(trip >= tT){
-                return true;
-            }
-        }
-        return false;
-
-    }
-}
+// Last updated: 12/26/2025, 12:11:43 PM
+1class Solution {
+2    public long minimumTime(int[] time, int totalTrips) {
+3        return answer(time, totalTrips);
+4    }
+5
+6    public long answer(int[] time, int tT){
+7        Arrays.sort(time);
+8        long low = time[0];
+9        long high = (long)tT * time[0];
+10        
+11        long ans = high;
+12
+13        while(low<=high){
+14            long mid = low - (low-high)/2;
+15            if(isPossible(time, mid, tT)){
+16                ans = mid;
+17                high = mid-1;
+18            }else{
+19                low = mid+1;
+20            }
+21        }
+22        return ans;
+23    }
+24
+25    public boolean isPossible(int[] time ,long mid, int tT){
+26        long trip = 0;
+27        for(int t : time){
+28            trip += mid/t;
+29            if(trip >= tT){
+30                return true;
+31            }
+32        }
+33        return false;
+34
+35    }
+36}
