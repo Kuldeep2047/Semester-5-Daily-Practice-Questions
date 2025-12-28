b868509a2b4ed81f0c29a467b887c6afb9717340
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-12-28 18:58:27 +0530

    File updated at 12/28/2025, 6:58:28 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,24 +1,24 @@
-// Last updated: 10/6/2025, 11:09:27 AM
-class Solution {
-    public int climbStairs(int n) {
-        int[] dp = new int[n];
-        Arrays.fill(dp ,-1);
-
-        return answer(n, dp, 0);
-    }
-    public int answer(int n, int[] dp ,int idx){
-        if(idx == n){
-            return 1;
-        }
-        if(idx > n){
-            return 0;
-        }
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int a = answer(n , dp ,idx+1);
-        int b = answer(n, dp, idx+2);
-        return dp[idx] = a+b;
-
-    }
-}
+// Last updated: 12/28/2025, 6:58:28 PM
+1class Solution {
+2    public int climbStairs(int n) {
+3        int[] dp = new int[n];
+4        Arrays.fill(dp ,-1);
+5
+6        return answer(n, dp, 0);
+7    }
+8    public int answer(int n, int[] dp ,int idx){
+9        if(idx == n){
+10            return 1;
+11        }
+12        if(idx > n){
+13            return 0;
+14        }
+15        if(dp[idx] != -1){
+16            return dp[idx];
+17        }
+18        int a = answer(n , dp ,idx+1);
+19        int b = answer(n, dp, idx+2);
+20        return dp[idx] = a+b;
+21
+22    }
+23}
