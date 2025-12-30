7f53110320a1d76c57f8c53bbcdb7d70b1be8246
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2025-12-30 11:39:50 +0530

    File updated at 12/30/2025, 11:39:49 AM

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,32 +1,29 @@
-// Last updated: 12/3/2025, 10:06:01 PM
+// Last updated: 12/30/2025, 11:39:49 AM
 1class Solution {
 2    public int numIslands(char[][] grid) {
-3        int ans =0;
-4        int n = grid.length;
-5        int m = grid[0].length;
-6
-7        for(int i=0;i<n;i++){
-8            for(int j=0;j<m;j++){
-9                if(grid[i][j]=='1'){
-10                    Islands(grid,i,j);
-11                    ans++;
-12                }
-13            }
-14        }
-15
-16        return ans;
-17    }
-18
-19    public static void Islands(char[][] grid,int cr,int cc){
-20        if(cc<0 || cr<0 || cr>=grid.length || cc>=grid[0].length || grid[cr][cc]!='1'){
-21            return;
-22        }
-23
-24        grid[cr][cc] = '*';
-25
-26        Islands(grid,cr+1,cc);
-27        Islands(grid,cr-1,cc);
-28        Islands(grid,cr,cc+1);
-29        Islands(grid,cr,cc-1);
-30    }
-31}
+3        int count =0;
+4
+5        for(int i=0 ;i<grid.length ;i++){
+6            for(int j=0 ;j<grid[0].length ;j++){
+7                if(grid[i][j] == '1'){
+8                    Island(grid, i,j);
+9                    count++;
+10                }
+11            }
+12        }
+13        return count;
+14        
+15    }
+16    public void Island(char[][] grid, int r, int c){
+17        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c] != '1'){
+18            return;
+19        }
+20
+21        grid[r][c] = '*';
+22
+23        Island(grid, r+1, c);
+24        Island(grid, r, c-1);
+25        Island(grid, r-1, c);
+26        Island(grid, r, c+1);
+27    }
+28}
