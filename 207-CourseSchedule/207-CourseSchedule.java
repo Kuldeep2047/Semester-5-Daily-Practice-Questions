3f4004b6fdafd65cb35dd9c5b0b76bcd3b34c0dc
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-02 00:39:40 +0530

    File updated at 1/2/2026, 12:39:39 AM

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,4 +1,4 @@
-// Last updated: 12/31/2025, 12:00:12 PM
+// Last updated: 1/2/2026, 12:39:39 AM
 1class Solution {
 2    public boolean canFinish(int numCourses, int[][] prerequisites) {
 3        return topological_sort(numCourses, prerequisites);
@@ -15,36 +15,37 @@
 14            map.get(edges[i][0]).add(edges[i][1]);
 15        }
 16        
-17        int[] in = new int[V];
-18        
-19        for(int key : map.keySet()){
-20            for(int i : map.get(key)){
-21                in[i]++;
-22            }
-23        }
-24        
-25        Queue<Integer> q= new LinkedList<>();
-26        ArrayList<Integer> ll = new ArrayList<>();
-27        
-28        for(int i=0 ;i<in.length ;i++){
-29            if(in[i] == 0){
-30                q.add(i);
-31            }
-32        }
-33        
-34        while(!q.isEmpty()){
-35            int r = q.poll();
-36            
-37            ll.add(r);
-38            for(int nbrs : map.get(r)){
-39                in[nbrs]--;
-40                
-41                if(in[nbrs] == 0){
-42                    q.add(nbrs);
-43                }
-44            }
-45        }
-46        
-47        return ll.size() == V;
-48    }
-49}
+17        //get indegree
+18        int[] in = new int[V];
+19        
+20        for(int key : map.keySet()){
+21            for(int i : map.get(key)){
+22                in[i]++;
+23            }
+24        }
+25        
+26        Queue<Integer> q= new LinkedList<>();
+27        ArrayList<Integer> ll = new ArrayList<>();
+28        
+29        for(int i=0 ;i<in.length ;i++){
+30            if(in[i] == 0){
+31                q.add(i);
+32            }
+33        }
+34        
+35        while(!q.isEmpty()){
+36            int r = q.poll();
+37            
+38            ll.add(r);
+39            for(int nbrs : map.get(r)){
+40                in[nbrs]--;
+41                
+42                if(in[nbrs] == 0){
+43                    q.add(nbrs);
+44                }
+45            }
+46        }
+47        
+48        return ll.size() == V;
+49    }
+50}
