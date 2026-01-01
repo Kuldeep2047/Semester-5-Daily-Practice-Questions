9ac3614387c424718cff1c092c9dadc837b75e8f
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-01 11:52:43 +0530

    File updated at 1/1/2026, 11:52:42 AM

--- 37-SudokuSolver/37-SudokuSolver.java
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -1,59 +1,59 @@
-// Last updated: 10/4/2025, 2:39:32 PM
-class Solution {
-    public void solveSudoku(char[][] board) {
-        fill_Suduko(board, 0, 0);
-    }
-
-    public boolean fill_Suduko(char[][] board, int row ,int col){
-        if(col ==9){
-            row++;
-            col =0;
-        }
-        if(row == 9){
-            return true;
-        }
-        if(board[row][col] != '.'){
-            return fill_Suduko(board, row ,col+1);
-        }else{
-            for(int val =1 ;val<=9;val++){
-                if(isitsafe(board, row, col, val)){
-                    board[row][col] = (char)(val+'0');
-                    boolean ans = fill_Suduko(board, row, col+1);
-                    if(ans){
-                        return true;
-                    }
-                    board[row][col] = '.';
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean isitsafe(char[][] board, int row, int col, int val){
-        //row
-        char ch = (char)(val+'0');
-        for(int i =0 ;i<board.length ;i++){
-            if(board[i][col] == ch){
-                return false;
-            }
-        }
-
-        for(int i =0 ;i<board[0].length ;i++){
-            if(board[row][i] == ch){
-                return false;
-            }
-        }
-
-        int r = row - row%3;
-        int c = col - col%3;
-        for(int i= r ;i< r+3 ;i++){
-            for(int j = c ;j< c+3 ;j++){
-                if(board[i][j] == ch){
-                    return false;
-                }
-            }
-        }
-        return true;
-
-    }
-}
+// Last updated: 1/1/2026, 11:52:42 AM
+1class Solution {
+2    public void solveSudoku(char[][] board) {
+3        fill_Suduko(board, 0, 0);
+4    }
+5
+6    public boolean fill_Suduko(char[][] board, int row ,int col){
+7        if(col ==9){
+8            row++;
+9            col =0;
+10        }
+11        if(row == 9){
+12            return true;
+13        }
+14        if(board[row][col] != '.'){
+15            return fill_Suduko(board, row ,col+1);
+16        }else{
+17            for(int val =1 ;val<=9;val++){
+18                if(isitsafe(board, row, col, val)){
+19                    board[row][col] = (char)(val+'0');
+20                    boolean ans = fill_Suduko(board, row, col+1);
+21                    if(ans){
+22                        return true;
+23                    }
+24                    board[row][col] = '.';
+25                }
+26            }
+27        }
+28        return false;
+29    }
+30
+31    public boolean isitsafe(char[][] board, int row, int col, int val){
+32        //row
+33        char ch = (char)(val+'0');
+34        for(int i =0 ;i<board.length ;i++){
+35            if(board[i][col] == ch){
+36                return false;
+37            }
+38        }
+39
+40        for(int i =0 ;i<board[0].length ;i++){
+41            if(board[row][i] == ch){
+42                return false;
+43            }
+44        }
+45
+46        int r = row - row%3;
+47        int c = col - col%3;
+48        for(int i= r ;i< r+3 ;i++){
+49            for(int j = c ;j< c+3 ;j++){
+50                if(board[i][j] == ch){
+51                    return false;
+52                }
+53            }
+54        }
+55        return true;
+56
+57    }
+58}
