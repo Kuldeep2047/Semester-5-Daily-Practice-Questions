faa21a9f41363ea76054cb4babe4a3840e2ee83f
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-02 11:31:41 +0530

    File updated at 1/2/2026, 11:31:41 AM

--- 133-CloneGraph/133-CloneGraph.java
+++ 133-CloneGraph/133-CloneGraph.java
@@ -1,4 +1,4 @@
-// Last updated: 1/2/2026, 11:16:29 AM
+// Last updated: 1/2/2026, 11:31:41 AM
 1/*
 2// Definition for a Node.
 3class Node {
@@ -24,30 +24,24 @@
 23        if(node == null){
 24            return null;
 25        }
-26        HashMap<Node, Node> map = new HashMap<>();
-27        Queue<Node> q= new LinkedList<>();
-28        
+26
+27        HashMap<Node, Node> map = new HashMap<>();
+28        Queue<Node> q=  new LinkedList<>();
 29
-30        // visites.add(node);
+30        q.add(node);
 31        map.put(node, new Node(node.val));
-32        q.add(node);
-33
-34        while(!q.isEmpty()){
-35            Node r = q.poll();
-36
-37            
-38
-39            for(Node nbrs : r.neighbors){
-40                if(!map.containsKey(nbrs)){
-41                    map.put(nbrs, new Node(nbrs.val));
-42                    q.add(nbrs);
-43                }
-44
-45                map.get(r).neighbors.add(map.get(nbrs));
-46            }
-47        }
-48        return map.get(node);
-49
-50        
-51    }
-52}
+32
+33        while(!q.isEmpty()){
+34            Node r = q.poll();
+35
+36            for(Node nbrs : r.neighbors){
+37                if(!map.containsKey(nbrs)){
+38                    map.put(nbrs, new Node(nbrs.val));
+39                    q.add(nbrs);
+40                }
+41                map.get(r).neighbors.add(map.get(nbrs));
+42            }
+43        }
+44        return map.get(node);
+45    }
+46}
