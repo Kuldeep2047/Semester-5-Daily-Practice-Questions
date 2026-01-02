28ca7525f4870946e88dba687768d34f9d6e0714
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-02 11:52:35 +0530

    File updated at 1/2/2026, 11:52:35 AM

--- 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.java
+++ 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.java
@@ -1,30 +1,27 @@
-// Last updated: 11/15/2025, 9:27:25 PM
-class Solution {
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        int n = graph.length;
-        
-
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ll = new ArrayList<>();
-
-        ll.add(0);
-        answer(0, n-1,graph, ans, ll) ;
-        return ans;
-    }
-
-    public static void answer(int src, int des, int[][] graph , List<List<Integer>> ans, List<Integer> ll){
-        if(src == des){
-            ans.add(new ArrayList<>(ll));
-            return;
-        }
-
-        for(int nbrs : graph[src]){
-            ll.add(nbrs);
-            answer(nbrs, des, graph, ans, ll);
-
-            ll.remove(ll.size()-1);
-        }
-
-
-    }
-}
+// Last updated: 1/2/2026, 11:52:35 AM
+1class Solution {
+2    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+3        int v = graph.length;
+4        List<List<Integer>> ans = new ArrayList<>();
+5        List<Integer> ll = new ArrayList<>();
+6        int src = 0;
+7        int des = v-1;
+8
+9        ll.add(src);
+10        answer(graph,ans, ll, src,des);
+11        return ans;
+12    }
+13
+14    public void answer(int[][] graph, List<List<Integer>> ans, List<Integer> ll, int src, int des){
+15        if(src == des){
+16            ans.add(new ArrayList<>(ll));
+17            return ;
+18        }
+19
+20        for(int nbrs : graph[src]){
+21            ll.add(nbrs);
+22            answer(graph, ans, ll, nbrs, des);
+23            ll.remove(ll.size()-1);
+24        }
+25    }
+26}
