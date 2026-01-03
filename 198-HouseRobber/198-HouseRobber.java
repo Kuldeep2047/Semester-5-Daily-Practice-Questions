e407dcd633b87168aece4cd97ff9d7e7ebfffe6f
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-03 12:53:21 +0530

    File updated at 1/3/2026, 12:53:21 PM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,37 +1,37 @@
-// Last updated: 10/6/2025, 11:44:37 AM
-class Solution {
-    public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp ,-1);
-        return answer(nums);
-        // return Max_Rob(nums, 0 , dp);
-    }
-
-    // public static int Max_Rob(int[] arr, int idx, int[] dp){
-    //     if(idx >= arr.length){
-    //         return 0;
-    //     }
-
-    //     if(dp[idx] != -1){
-    //         return dp[idx];
-    //     }
-
-    //     int not_rob = Max_Rob(arr, idx +1, dp);
-    //     int rob = arr[idx] + Max_Rob(arr, idx +2, dp);
-    //     return dp[idx] = Math.max(not_rob, rob);
-    // }
-
-    public int answer(int[] arr){
-        int prev2 =0;
-        int prev =arr[0];
-        for(int i=1;i<arr.length ;i++){
-            int pick = arr[i] + prev2;
-            int not_pick = prev;
-            int curr = Math.max(pick, not_pick);
-
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
-    }
-}
+// Last updated: 1/3/2026, 12:53:21 PM
+1class Solution {
+2    public int rob(int[] nums) {
+3        int[] dp = new int[nums.length];
+4        Arrays.fill(dp ,-1);
+5        // return answer(nums);
+6        return Max_Rob(nums, 0 , dp);
+7    }
+8
+9    public static int Max_Rob(int[] arr, int idx, int[] dp){
+10        if(idx >= arr.length){
+11            return 0;
+12        }
+13
+14        if(dp[idx] != -1){
+15            return dp[idx];
+16        }
+17
+18        int not_rob = Max_Rob(arr, idx +1, dp);
+19        int rob = arr[idx] + Max_Rob(arr, idx +2, dp);
+20        return dp[idx] = Math.max(not_rob, rob);
+21    }
+22
+23    // public int answer(int[] arr){
+24    //     int prev2 =0;
+25    //     int prev =arr[0];
+26    //     for(int i=1;i<arr.length ;i++){
+27    //         int pick = arr[i] + prev2;
+28    //         int not_pick = prev;
+29    //         int curr = Math.max(pick, not_pick);
+30
+31    //         prev2 = prev;
+32    //         prev = curr;
+33    //     }
+34    //     return prev;
+35    // }
+36}
