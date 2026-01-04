54ad89038539f52295e9d5a3ebc7785b71fbead9
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-04 12:24:09 +0530

    File updated at 1/4/2026, 12:24:09 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,24 +1,25 @@
-// Last updated: 12/25/2025, 10:16:03 PM
+// Last updated: 1/4/2026, 12:24:09 PM
 1class Solution {
 2    public int lengthOfLIS(int[] nums) {
 3        return answer(nums);
 4    }
-5    public int answer(int[] arr){
-6        int n = arr.length;
-7        int[] dp = new int[n];
-8        Arrays.fill(dp ,1);
-9        int max =1;
+5
+6    public int answer(int[] arr){
+7        int n = arr.length;
+8        int[] dp = new int[n];
+9        int ans =1;
 10
-11        for(int i=1 ;i<n ;i++){
-12            for(int j=i-1 ;j>=0 ;j--){
-13                if(arr[i] > arr[j]){
-14                    dp[i] = Math.max(dp[i], dp[j]+1);
-15
-16                    max = Math.max(max, dp[i]);
-17                }
-18            }
-19        }
-20        return max;
+11        Arrays.fill(dp , 1);
+12
+13        for(int i=0 ;i<n ;i++){
+14            for(int j = i-1 ;j>=0 ;j--){
+15                if(arr[i]>arr[j]){
+16                    dp[i] = Math.max(dp[i], dp[j] +1);
+17                    ans = Math.max(ans, dp[i]);
+18                }                
+19            }
+20        }
 21
-22    }
-23}
+22        return ans;
+23    }
+24}
