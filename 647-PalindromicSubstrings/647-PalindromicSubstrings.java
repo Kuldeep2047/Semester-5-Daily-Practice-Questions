53dea6e0cc1ac63b0ff07533d2968f97420f9819
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-04 11:40:01 +0530

    File updated at 1/4/2026, 11:40:01 AM

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.java
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -1,29 +1,34 @@
-// Last updated: 12/5/2025, 10:52:59 PM
+// Last updated: 1/4/2026, 11:40:01 AM
 1class Solution {
 2    public int countSubstrings(String s) {
-3        return Count_Palindrome(s);
-4    }
-5    public static int Count_Palindrome(String s) {
-6		int odd =0;
-7		
-8		for(int axis=0; axis<s.length(); axis++) {
-9			for(int orbit=0; axis-orbit>=0 && axis+orbit<s.length(); orbit++) {
-10				if(s.charAt(axis-orbit) != s.charAt(axis+orbit)) {
-11					break;
-12				}
-13				odd++;
-14			}
-15		}
-16		int even=0;
-17		for (double axis = 0.5; axis < s.length(); axis++) {
-18			for (double orbit = 0.5; axis - orbit >= 0 && axis + orbit < s.length(); orbit++) {
-19				if (s.charAt((int) (axis - orbit)) != s.charAt((int) (axis + orbit))) {
-20					break;
-21				}
-22				even++;
-23			}
-24		}
-25		return odd+even;
-26		
-27	}
-28}
+3        return answer(s);
+4        
+5    }
+6
+7    public int answer(String s){
+8        int n = s.length();
+9        int ans =0;
+10
+11        for(int axis =0 ;axis<n ; axis++){
+12            for(int orbit =0 ; (axis-orbit) >=0 && (axis + orbit) < n ; orbit++){
+13                if(s.charAt(axis-orbit) == s.charAt(axis + orbit)){
+14                    ans++;
+15                }else{
+16                    break;
+17                }
+18            }
+19        }
+20
+21        for(double axis =0.5 ;axis<n ; axis++){
+22            for(double orbit =0.5 ; axis-orbit >=0 && axis + orbit < n ; orbit++){
+23                if(s.charAt((int)(axis-orbit)) == s.charAt((int)(axis + orbit))){
+24                    ans++;
+25                }else{
+26                    break;
+27                }
+28            }
+29        }
+30
+31        return ans;
+32    }
+33}
