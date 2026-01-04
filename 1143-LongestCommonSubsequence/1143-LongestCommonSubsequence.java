34048445f71d9a9bb4ced0b82ef85920bdccba8c
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-04 11:12:15 +0530

    File updated at 1/4/2026, 11:12:14 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,32 +1,29 @@
-// Last updated: 12/25/2025, 11:35:30 PM
+// Last updated: 1/4/2026, 11:12:14 AM
 1class Solution {
 2    public int longestCommonSubsequence(String text1, String text2) {
 3        int[][] dp = new int[text1.length()][text2.length()];
-4        for(int[] arr : dp){
-5            Arrays.fill(arr, -1);
+4        for(int[] a : dp){
+5            Arrays.fill(a, -1);
 6        }
-7
-8        return LCS(text1, text2, 0,0,dp);
-9    }
-10
-11    public int LCS(String s1 ,String s2, int i,int j, int[][] dp){
-12        if(i == s1.length() || j == s2.length()){
-13            return 0;
-14        }
-15        if(dp[i][j] != -1){
-16            return dp[i][j];
-17        }
-18
-19        int ans =0;
-20        if(s1.charAt(i) == s2.charAt(j)){
-21            ans = 1+ LCS(s1,s2,i+1,j+1, dp);
-22        }
-23        else{
-24            int a = LCS(s1,s2,i,j+1, dp);
-25            int b = LCS(s1,s2,i+1,j, dp);
-26            ans = Math.max(a,b);
-27        }
-28
-29        return dp[i][j] = ans;
-30    }
-31}
+7        return answer(text1, text2, 0,0, dp);
+8    }
+9    public int answer(String s1, String s2, int i, int j, int[][] dp){
+10        if(i == s1.length() || j == s2.length()){
+11            return 0;
+12        }
+13
+14        if(dp[i][j] != -1){
+15            return dp[i][j];
+16        }
+17        int ans =0;
+18        if(s1.charAt(i) == s2.charAt(j)){
+19            ans = 1 + answer(s1, s2, i+1, j+1, dp);
+20        }else{
+21            int a= answer(s1, s2, i, j+1, dp);
+22            int b = answer(s1, s2, i+1, j, dp); 
+23            ans = Math.max(a,b);
+24        }
+25        return  dp[i][j] = ans;
+26    }
+27
+28}
