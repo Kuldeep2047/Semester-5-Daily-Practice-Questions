ea03377fa361e768a1572e636f9227c239568f8b
Kuldeep Garg <kuldeepgarg6993@gmail.com>
2026-01-04 11:23:18 +0530

    File updated at 1/4/2026, 11:23:17 AM

--- 583-DeleteOperationforTwoStrings/583-DeleteOperationforTwoStrings.java
+++ 583-DeleteOperationforTwoStrings/583-DeleteOperationforTwoStrings.java
@@ -1,4 +1,4 @@
-// Last updated: 12/21/2025, 1:51:57 PM
+// Last updated: 1/4/2026, 11:23:17 AM
 1class Solution {
 2    public int minDistance(String word1, String word2) {
 3        return answer(word1, word2);
@@ -15,26 +15,27 @@
 14        int lcs = LCS(s1,s2,0,0,dp);
 15
 16        return (n- lcs) + (m- lcs);
-17
+17      
 18
-19    }
-20
-21    public static int LCS(String s1, String s2, int i, int j, int[][] dp) {
-22		if (i == s1.length() || j == s2.length()) {
-23			return 0;
-24		}
-25		if (dp[i][j] != -1) {
-26			return dp[i][j];
-27		}
-28		int ans = 0;
-29		if (s1.charAt(i) == s2.charAt(j)) {
-30			ans = 1 + LCS(s1, s2, i + 1, j + 1, dp);
-31		} else {
-32			int f = LCS(s1, s2, i + 1, j, dp);
-33			int s = LCS(s1, s2, i, j + 1, dp);
-34			ans = Math.max(f, s);
-35
-36		}
-37		return dp[i][j] = ans;
-38	}
-39}
+19
+20    }
+21
+22    public static int LCS(String s1, String s2, int i, int j, int[][] dp) {
+23		if (i == s1.length() || j == s2.length()) {
+24			return 0;
+25		}
+26		if (dp[i][j] != -1) {
+27			return dp[i][j];
+28		}
+29		int ans = 0;
+30		if (s1.charAt(i) == s2.charAt(j)) {
+31			ans = 1 + LCS(s1, s2, i + 1, j + 1, dp);
+32		} else {
+33			int f = LCS(s1, s2, i + 1, j, dp);
+34			int s = LCS(s1, s2, i, j + 1, dp);
+35			ans = Math.max(f, s);
+36
+37		}
+38		return dp[i][j] = ans;
+39	}
+40}
